a39ba39a096b5c4c51337f81784b051d1634391a
xianweihuihuan <2907404984@qq.com>
2024-12-05 09:53:15 +0800

    1111

--- exercise/my_ls.c
+++ exercise/my_ls.c
@@ -1,6 +1,39 @@
 #include "my_ls.h"
 
 int mode[1000] = {0};
+void stackinit(stack *st)
+{
+    st->file = NULL;
+    st->alloc = 0;
+    st->sz = 0;
+}
+void boarden(stack *st)
+{
+    int newn = (st->alloc == 0) ? 4 : st->alloc * 2;
+    st->file = (char **)realloc(st->file, sizeof(char *));
+    st->alloc = newn;
+    for (int i = st->sz; i < st->alloc; i++)
+    {
+        st->file[i] = (char *)malloc(100);
+    }
+}
+void stackpush(stack *st, const char *file)
+{
+    if (st->sz == st->alloc)
+    {
+        boarden(st);
+    }
+    strcpy(st->file[st->sz], file);
+    st->sz += 1;
+}
+void stackpop(stack *st)
+{
+    st->sz -= 1;
+}
+bool stackempty(stack *st)
+{
+    return (st->sz == 0);
+}
 int sort1(const void *a, const void *b)
 {
     int flag = 1;
@@ -39,7 +72,6 @@ int sort1(const void *a, const void *b)
         return (aa - bb) * flag;
     }
 }
-
 void ls_none(int argc, char *argv[])
 {
     int index = 0;
@@ -92,7 +124,6 @@ int sort_time(const void *a, const void *b)
     struct fm *tmp2 = (struct fm *)b;
     return ((tmp2->stat.st_mtime) - (tmp1->stat.st_mtime)) * flag;
 }
-
 void ls_normal(struct fm *name, long count)
 {
     if (mode['a'] == 0)
@@ -194,137 +225,73 @@ void ls_normal(struct fm *name, long count)
         }
     }
 }
-
 void ls_l(struct fm *name, long count)
 {
-    if (mode['a'] == 0)
+    static char *perm[] = {"---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"};
+    if (mode['s'] != 0)
     {
-        static char *perm[] = {"---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"};
-        if (mode['s'] != 0)
-        {
-            int tmp = 0;
-            for (int i = 0; i < count; i++)
-            {
-                if (name[i].file.d_name[0] == '.')
-                {
-                    continue;
-                }
-                tmp += name[i].stat.st_blocks;
-            }
-            printf("总计 %d\n", tmp / 2);
-        }
+        int tmp = 0;
         for (int i = 0; i < count; i++)
         {
-            if (name[i].file.d_name[0] == '.')
+            if (mode['a'] == 0 && name[i].file.d_name[0] == '.')
             {
                 continue;
             }
-            int mask = 0700;
-            if (mode['i'] != 0)
-            {
-                printf("%7ld ", name[i].stat.st_ino);
-            }
-            if (mode['s'] != 0)
-            {
-                printf("%-3ld", name[i].stat.st_blocks / 2);
-            }
-            if (name[i].file.d_type == 4)
-            {
-                printf("d");
-            }
-            else
-            {
-                printf("-");
-            }
-            for (int j = 3; j; --j)
-            {
-                printf("%3s", perm[(name[i].stat.st_mode & mask) >> (j - 1) * N_BITS]);
-                mask >>= N_BITS;
-            }
-            printf(" ");
-            printf("%ld ", name[i].stat.st_nlink);
-            struct passwd *aaa = getpwuid(name[i].stat.st_uid);
-            printf("%s ", aaa->pw_name);
-            struct group *bbb = getgrgid(name[i].stat.st_gid);
-            printf("%s ", bbb->gr_name);
-            printf("%10ld ", name[i].stat.st_size);
-            struct tm *ccc = localtime(&(name[i].stat.st_mtime));
-            printf("%2d月 %2d %-2d:%-2d ", ccc->tm_mon + 1, ccc->tm_mday, ccc->tm_hour, ccc->tm_min);
-            if (name[i].file.d_type == 4)
-            {
-                printf("\033[0m\033[1;36m%-20s\033[m\n", name[i].file.d_name);
-            }
-            if ((name[i].file.d_type == 8))
-            {
-                printf("\033[0m\033[1;33m%-20s\033[m\n", name[i].file.d_name);
-            }
-            if (name[i].file.d_type == 10)
-            {
-                printf("\033[0m\033[1;32m%-20s\033[m\n", name[i].file.d_name);
-            }
+            tmp += name[i].stat.st_blocks;
         }
+        printf("总计 %d\n", tmp / 2);
     }
-    else
+    for (int i = 0; i < count; i++)
     {
-        static char *perm[] = {"---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"};
+        if (mode['a'] == 0 && name[i].file.d_name[0] == '.')
+        {
+            continue;
+        }
+        int mask = 0700;
+        if (mode['i'] != 0)
+        {
+            printf("%7ld ", name[i].stat.st_ino);
+        }
         if (mode['s'] != 0)
         {
-            int tmp = 0;
-            for (int i = 0; i < count; i++)
-            {
-                tmp += name[i].stat.st_blocks;
-            }
-            printf("总计 %d\n", tmp / 2);
+            printf("%-3ld", name[i].stat.st_blocks / 2);
         }
-        for (int i = 0; i < count; i++)
+        if (name[i].file.d_type == 4)
         {
-            int mask = 0700;
-            if (mode['i'] != 0)
-            {
-                printf("%7ld ", name[i].stat.st_ino);
-            }
-            if (mode['s'] != 0)
-            {
-                printf("%-3ld", name[i].stat.st_blocks / 2);
-            }
-            if (name[i].file.d_type == 4)
-            {
-                printf("d");
-            }
-            else
-            {
-                printf("-");
-            }
-            for (int j = 3; j; --j)
-            {
-                printf("%3s", perm[(name[i].stat.st_mode & mask) >> (j - 1) * N_BITS]);
-                mask >>= N_BITS;
-            }
-            printf(" ");
-            printf("%ld ", name[i].stat.st_nlink);
-            struct passwd *aaa = getpwuid(name[i].stat.st_uid);
-            printf("%s ", aaa->pw_name);
-            struct group *bbb = getgrgid(name[i].stat.st_gid);
-            printf("%s ", bbb->gr_name);
-            printf("%10ld ", name[i].stat.st_size);
-            struct tm *ccc = localtime(&(name[i].stat.st_mtime));
-            printf("%2d月 %2d %-2d:%-2d ", ccc->tm_mon + 1, ccc->tm_mday, ccc->tm_hour, ccc->tm_min);
-            if (name[i].file.d_type == 4)
-            {
-                printf("\033[0m\033[1;36m%-20s\033[m\n", name[i].file.d_name);
-            }
-            if ((name[i].file.d_type == 8))
-            {
-                printf("\033[0m\033[1;33m%-20s\033[m\n", name[i].file.d_name);
-            }
-            if (name[i].file.d_type == 10)
-            {
-                printf("\033[0m\033[1;32m%-20s\033[m\n", name[i].file.d_name);
-            }
+            printf("d");
+        }
+        else
+        {
+            printf("-");
+        }
+        for (int j = 3; j; --j)
+        {
+            printf("%3s", perm[(name[i].stat.st_mode & mask) >> (j - 1) * N_BITS]);
+            mask >>= N_BITS;
+        }
+        printf(" ");
+        printf("%ld ", name[i].stat.st_nlink);
+        struct passwd *aaa = getpwuid(name[i].stat.st_uid);
+        printf("%s ", aaa->pw_name);
+        struct group *bbb = getgrgid(name[i].stat.st_gid);
+        printf("%s ", bbb->gr_name);
+        printf("%10ld ", name[i].stat.st_size);
+        struct tm *ccc = localtime(&(name[i].stat.st_mtime));
+        printf("%2d月 %2d %-2d:%-2d ", ccc->tm_mon + 1, ccc->tm_mday, ccc->tm_hour, ccc->tm_min);
+        if (name[i].file.d_type == 4)
+        {
+            printf("\033[0m\033[1;36m%-20s\033[m\n", name[i].file.d_name);
+        }
+        if ((name[i].file.d_type == 8))
+        {
+            printf("\033[0m\033[1;33m%-20s\033[m\n", name[i].file.d_name);
+        }
+        if (name[i].file.d_type == 10)
+        {
+            printf("\033[0m\033[1;32m%-20s\033[m\n", name[i].file.d_name);
         }
     }
 }
-
 void ls_R(struct fm *name, long count)
 {
     ;
