b85c57328238e0de7218ab34d3b3417a8ea4fd8a
xianweihuihuan <2907404984@qq.com>
2024-12-10 18:49:19 +0800

    1111

--- exercise/progresstest.c
+++
@@ -1,9 +0,0 @@
-#include"progress.h"
-
-int main(){
-    download(1024,progress);
-    download(512,progress);
-    download(256,progress);
-    download(128,progress);
-    download(64,progress);
-}
