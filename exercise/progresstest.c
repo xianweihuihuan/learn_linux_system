a72624d4370237b5ab8ed0befa46803d2f2b765f
xianweihuihuan <2907404984@qq.com>
2024-12-10 18:37:23 +0800

    进度条测试文件

---
+++ exercise/progresstest.c
@@ -0,0 +1,9 @@
+#include"progress.h"
+
+int main(){
+    download(1024,progress);
+    download(512,progress);
+    download(256,progress);
+    download(128,progress);
+    download(64,progress);
+}
