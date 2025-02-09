bc4378c5d934488a631e2037be62e121f8e86e80
xianweihuihuan <2907404984@qq.com>
2025-02-09 23:58:08 +0800

    myshell——测试文件

--- my_shell/my_shelltest.cpp
+++ my_shell/my_shelltest.cpp
@@ -4,23 +4,33 @@ int main()
 {
     extern std::string infile;
     extern std::string outfile;
-    Env_Init();
+    //Env_Init();
     while (true)
     {
         Print_commandline();
         char commandline[COMMAND_SIZE];
-        if(!getcommand(commandline,COMMAND_SIZE)){
+        if (!getcommand(commandline, COMMAND_SIZE))
+        {
             continue;
+        }
+        if (strncmp(commandline, "quit",4)==0)
+        {
+            break;
         }
         checkredir(commandline);
         checkfifo(commandline);
-        //fifotest();
-        //checktest();
+        // fifotest();
+        // checktest();
+        if (checkselfexcute())
+        {
+            infile.clear();
+            outfile.clear();
+            continue;
+        }
         infile.clear();
         outfile.clear();
-        
     }
-//     for(int i = 0;environ[i];i++){
-//         std::cout<<environ[i]<<std::endl;
-//     }
+    //     for(int i = 0;environ[i];i++){
+    //         std::cout<<environ[i]<<std::endl;
+    //     }
 }
