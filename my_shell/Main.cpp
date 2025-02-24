21313fa9a3ee75813353429bb7a6545f87cc808f
xianweihuihuan <2907404984@qq.com>
2025-02-24 20:16:52 +0800

    myshell——main

--- my_shell/Main.cpp
+++ my_shell/Main.cpp
@@ -4,9 +4,8 @@ int main()
 {
     extern std::string infile;
     extern std::string outfile;
-    //Env_Init();
     signal(SIGINT, SIG_IGN);
-    std::cout<<Cyan<<"Welcome to user my shell"<<Tail<<std::endl;
+    std::cout << Cyan << "Welcome to user my shell" << Tail << std::endl;
     while (true)
     {
         Print_commandline();
@@ -15,29 +14,20 @@ int main()
         {
             continue;
         }
-        //printf("%s\n",commandline);
-        if (strcmp(commandline, "exit")==0)
+        if (strcmp(commandline, "exit") == 0)
         {
             break;
         }
         checkredir(commandline);
         checkfifo(commandline);
-        // fifotest();
-        // checktest();
         if (checkselfexcute())
         {
-            //printf("1111111111\n");
             infile.clear();
             outfile.clear();
             continue;
         }
-        //printf("1111111111\n");
         excute();
         infile.clear();
         outfile.clear();
     }
-    
-    //     for(int i = 0;environ[i];i++){
-    //         std::cout<<environ[i]<<std::endl;
-    //     }
 }
